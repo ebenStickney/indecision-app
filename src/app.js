ed57e09108d3b2f5983040898993c0300b6f382c
ebenStickney <36087626+ebenStickney@users.noreply.github.com>
2018-04-03 11:09:05 -0700

    Setting up initial props
    Creating props for Header and Options

--- src/app.js
+++ src/app.js
@@ -1,2 +1,84 @@
+class Header extends React.Component {
+    render() {
+        return (
+        <div>
+          <h1>{this.props.title}</h1>
+          <h3> {this.props.subtitle}</h3>
+        </div>
+        )
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+        <div>
+          <button>What Should I Do?</button>
+        </div>
+        
+        )
+    }
+}
 
 
+class Options extends React.Component {
+    render() {
+        
+        return (
+        <div>
+          <ol>
+          {this.props.options.map((option) => {
+                    return  <Option key={option} optionText={option} />
+                }
+    
+)}
+         </ol>
+        </div>
+        
+        )
+    }
+}
+
+class Option extends React.Component {
+    render () {
+        return (
+         <div>
+            <li>{this.props.optionText}</li>
+         </div>
+        )
+    }
+}
+class AddOption extends React.Component {
+    render() {
+      return (
+      <div>
+        <form>
+          <input></input> 
+          <button>Add Option</button>
+        </form>
+      </div>
+      
+      )
+    }
+}
+
+class IndecisionApp extends React.Component {
+    render() {
+        
+        const title = "Indecision App";
+        const subtitle = "Put your Fate in the Hands of a Computer";
+        let options = ['thing 1', 'thing 2', 'thing 3'];
+        
+     return (
+        <div>
+          <Header title={title} subtitle={subtitle}/>
+          <Action />
+          <Options options={options}/>
+          <AddOption />
+        </div>
+        
+        )
+    }
+}
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
